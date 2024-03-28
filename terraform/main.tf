6fce4abdbbf1d5aaddbb143a0ea9857f7ccb667c
NRI酒井 将大 <m2-sakai@nri.co.jp>
2024-03-28 15:00:05 +0900

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_windows_function_app" "function_app" {
   resource_group_name = "${var.param_resource_group_name}"
   location            = "${var.param_location}"
 
-  storage_account_name       = azurerm_storage_account.name
+  storage_account_name       = azurerm_storage_account.storage_account.name
   storage_account_access_key = azurerm_storage_account.storage_account.primary_access_key
   service_plan_id            = azurerm_service_plan.app_service_plan.id
 
